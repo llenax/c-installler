3e05f33b8438f4c850cc58431855b99acc54f450
llenaX <jaimitr14@gmail.com>
2021-03-14 23:47:06 +0300

    style

--- docs/pages/_app.js
+++ docs/pages/_app.js
@@ -1,14 +1,6 @@
-import { useEffect } from "react";
-
 import "../styles.css";
 
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    window.onbeforeunload = function () {
-      window.scrollTo(0, 0);
-    };
-  }, []);
-
   return <Component {...pageProps} />;
 }
 
